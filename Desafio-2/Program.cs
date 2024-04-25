86370a59d5c1338b24327c054f7429b82187db44
Lucas Mateus <Lucas015x@gmail.com>
2024-04-25 19:32:15 -0300

    Funcao de soma de elemento em um array

---
+++ Desafio-2/Program.cs
@@ -0,0 +1,13 @@
+﻿static int SimpleArraySum(List<int> ar)
+{
+    int sum = 0;
+
+    for (int i = 0; i < ar.Count; i++)
+    {
+        sum += ar[i];
+    }
+
+    return sum;
+}
+
+Console.WriteLine(SimpleArraySum([4, 4, 4, 4]));
