bd2795eb832930c7de5aa835f2c26c9bb9214ba6
Lucas Mateus <Lucas015x@gmail.com>
2024-04-25 19:27:40 -0300

    Funcao de Soma de 2 numeros

---
+++ Desafio-1/Program.cs
@@ -0,0 +1,8 @@
+﻿static int SumTwoNumbers(int a, int b)
+{
+    return a + b;
+}
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumTwoNumbers(num1, num2));
