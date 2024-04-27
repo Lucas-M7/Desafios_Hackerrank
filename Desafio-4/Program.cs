ff07bd95ced1cb4fd55f4b3f2528493fdaea0fca
Lucas Mateus <Lucas015x@gmail.com>
2024-04-26 21:08:14 -0300

    Funcao que faz a soma de valores long em um array

---
+++ Desafio-4/Program.cs
@@ -0,0 +1,12 @@
+﻿static void AVeryBigSum(List<long> myAr)
+{
+    long result = 0;
+
+    for (int i = 0; i < myAr.Count; i++)
+    {
+        result += myAr[i];
+    }
+    Console.WriteLine(result);
+}
+
+AVeryBigSum([5, 1000000000000000000]);
