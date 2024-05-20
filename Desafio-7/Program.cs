f40015e7a07f209ea03d8279af43760228d4a731
Lucas Mateus <Lucas015x@gmail.com>
2024-05-20 19:23:38 -0300

    Desafio Staircase

---
+++ Desafio-7/Program.cs
@@ -0,0 +1,25 @@
+﻿// STAIRCASE
+
+Console.WriteLine("Digite um número entre 1 e 10.");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 0 || n < 11)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            Console.Write(' ');
+        }
+        for (int k = 0; k < i; k++)
+        {
+            Console.Write('#');
+        }
+
+        Console.WriteLine();
+    }
+}
+else
+{
+    Console.WriteLine("O número deve ser entre 1 e 10.");
+}
